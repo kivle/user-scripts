5be63b6c84d2e653d925319994b5157d1d24fa46
Helge Elvik <helge.elvik@gmail.com>
2024-08-12 09:03:34 +0200

    Update script to expose rss link on youtube

--- youtube-expose-rss-link.user.js
+++ youtube-expose-rss-link.user.js
@@ -3,40 +3,24 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://*.youtube.com/*
 // @grant       none
-// @version     1.0
+// @require     https://raw.githubusercontent.com/kivle/user-scripts/master/tools.js
+// @version     1.1
 // @author      -
 // @description 8/14/2022, 10:54:50 AM
 // ==/UserScript==
 
-console.log("Loading expose youtube rss link script");
+async function inject() {
+  const linkTag = await waitForElement("link[rel='alternate'][type='application/rss+xml']");
+  const rssLink = document.createElement("a");
+  rssLink.classList.add("rssLink");
+  rssLink.href = linkTag.href;
+  rssLink.textContent = "RSS";
+  rssLink.style.paddingLeft = "10px";
 
-function inject() {
-  const subscriberCountNode = document.querySelector(
-    "yt-formatted-string#subscriber-count"
-  );
-  if (subscriberCountNode && !document.querySelector("a.rssLink")) {
-    const linkTag = document.querySelector(
-      "link[rel='alternate'][type='application/rss+xml']"
-    );
-    if (linkTag) {
-      console.debug("Inserting rssLink");
-      const rssLink = document.createElement("a");
-      rssLink.classList.add("rssLink");
-      rssLink.href = linkTag.href;
-      rssLink.textContent = "RSS";
-      rssLink.style.paddingLeft = "10px";
-      subscriberCountNode.parentNode.append(rssLink);
-    }
-  }
-}
-
-async function main() {
+  const header = await waitForElement("div.page-header-view-model-wiz__page-header-headline > div > yt-dynamic-text-view-model > h1 > span");
+  header.append(rssLink);
+  await waitForElementToBeRemoved("a.rssLink");
   inject();
-  const observer = new MutationObserver(inject);
-  observer.observe(document, {
-    childList: true,
-    subtree: true,
-  });
 }
 
-main();
+inject();
